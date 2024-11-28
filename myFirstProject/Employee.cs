d988accd42c863a4f0981d1c06b2ea91e66b2e93
RayanMohammed <alqthami@pgd.gov.sa>
2024-11-28 10:00:59 +0300

    assignment6

--- myFirstProject/Employee.cs
+++ myFirstProject/Employee.cs
@@ -1,84 +1,137 @@
-﻿namespace myFirstProject
+﻿using System.Runtime.CompilerServices;
+
+namespace myFirstProject
 {
     internal class Employee
     {
         //Logical Representation, like blue print
         // the following are called (member fields)
         //hide/show [Encapsulation]
-        public int ID;
-       public  string Name;
-        public string address;
-        public int PhoneNumber;
-        private int Salary;
-      public void SetSalary(int s)
+       private int id;
+        private string name;
+        private string address;
+        private int phonenumber;
+        private int salary;
+
+      //public void SetSalary(int s)
+      //  {
+      //      if (s >6000) 
+      //      {
+      //          Salary = s; 
+      //      }
+      //      else
+      //      {
+      //          Salary = 6000;
+      //      }
+      //  }
+
+        //private property
+        public int Salary
         {
-            if (s >6000) 
+            get 
             {
-                Salary = s; 
+                return salary;
             }
-            else
+            set 
             {
-                Salary = 6000;
+                if(value>6000)
+                {
+                    salary = value;
+                }
+                else 
+                {
+                    Console.WriteLine("you salary is not more than 6000!");
+                }
             }
         }
-        //Display function
-      public  void Display()
+
+
+        //constructor
+public Employee()
+        {
+            Console.WriteLine("empty constructor!");
+        }
+
+        //constructor with id
+        public Employee(int id):this()
+        {
+            id = 0;
+            Console.WriteLine("id");
+        }
+        public Employee(int id, string name, int salary):this()
+        {
+
+            id = 1;
+            name = "rayan";
+            salary = 121212;
+            Console.WriteLine("id,name,salary");
+           
+        }
+        //display
+        public void Display()
         {
             Console.WriteLine("Employees Details.:-\n"
-                              +$"Name:{Name}|Salary:{Salary}|PhoneNumber:{PhoneNumber}");
+                              + $"ID:{id}|Name:{name}|Salary:{Salary}");
         }
-        public void addemployee()
-        {
-            string answer;
-            Console.WriteLine("do you want to add a new employee?(y/n)");
-            answer = Console.ReadLine();
-            if (answer == "y")
-            {
-            Console.WriteLine("Enter the number of employees:");
-            int numberOfEmployees = Convert.ToInt32(Console.ReadLine());
 
-            // Create an array to store employees
-            Employee[] employees = new Employee[numberOfEmployees];
+        //  //Display function
+        //public void Display()
+        //{
+        //    Console.WriteLine("Employees Details.:-\n"
+        //                      + $"ID:{id}|Name:{name}|Salary:{Salary}|PhoneNumber:{phonenumber}|Address:{address}");
+        //}
+        //public void addemployee()
+        //{
+        //    string answer;
+        //    Console.WriteLine("do you want to add a new employee?(y/n)");
+        //    answer = Console.ReadLine();
+        //    if (answer == "y")
+        //    {
+        //    Console.WriteLine("Enter the number of employees:");
+        //    int numberOfEmployees = Convert.ToInt32(Console.ReadLine());
 
-            // Populate the array with user input
-            for (int i = 0; i < numberOfEmployees; i++)
-            {
-                Console.WriteLine($"Enter details for employee {i + 1}:");
+        //    // Create an array to store employees
+        //    Employee[] employees = new Employee[numberOfEmployees];
 
-                Console.Write("ID: ");
-                int id = Convert.ToInt32(Console.ReadLine());
+        //    // Populate the array with user input
+        //    for (int i = 0; i < numberOfEmployees; i++)
+        //    {
+        //        Console.WriteLine($"Enter details for employee {i + 1}:");
 
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
+        //        Console.Write("ID: ");
+        //        int id = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Address: ");
-                string address = Console.ReadLine();
+        //        Console.Write("Name: ");
+        //        string name = Console.ReadLine();
 
-                Console.Write("Phonenumber: ");
-                int phonenumber = Convert.ToInt32(Console.ReadLine());
+        //        Console.Write("Address: ");
+        //        string address = Console.ReadLine();
 
-                Console.Write("Salary: ");
-                int salary = Convert.ToInt32(Console.ReadLine());
-                // Create a new Employee object and store it in the array
-                employees[i] = new Employee { ID = id, Name = name, address = address, PhoneNumber = phonenumber, Salary = salary };
-            }
-                SetSalary(Salary);
+        //        Console.Write("Phonenumber: ");
+        //        int phonenumber = Convert.ToInt32(Console.ReadLine());
 
-                // Display the entered employee details
-                Console.WriteLine("\nEmployee Details:");
-            foreach (var employee in employees)
-            {
-                Console.WriteLine($"ID: {employee.ID}, Name: {employee.Name}, Address: {employee.address}, PhoneNumber: {employee.PhoneNumber}, Salary: {employee.Salary}");
-            }
-            }
-            else if(answer == "n") 
-            {
-                Environment.Exit(0);
-            }
-            else
-            {
-                Console.WriteLine("Please choose correct letter");
-            }
-        }
+        //        Console.Write("Salary: ");
+        //        int salary = Convert.ToInt32(Console.ReadLine());
+        //        // Create a new Employee object and store it in the array
+        //        employees[i] = new Employee { ID = id, Name = name, address = address, PhoneNumber = phonenumber, Salary = salary };
+        //    }
+        //        SetSalary(Salary);
+
+        //        // Display the entered employee details
+        //        Console.WriteLine("\nEmployee Details:");
+        //    foreach (var employee in employees)
+        //    {
+        //        Console.WriteLine($"ID: {employee.ID}, Name: {employee.Name}, Address: {employee.address}, PhoneNumber: {employee.PhoneNumber}, Salary: {employee.Salary}");
+        //    }
+        //    }
+        //    else if(answer == "n") 
+        //    {
+        //        Environment.Exit(0);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Please choose correct letter");
+        //    }
+        //}
     }
 }
--- myFirstProject/Program.cs
+++ myFirstProject/Program.cs
@@ -69,23 +69,34 @@ namespace myFirstProject
         #endregion
         static void Main(string[] args)
         {
-            #region Assignment5
+
+
+            #region assignment6
+            Employee rayan = new Employee(1);
+            rayan.Display();
+
+
+            #endregion
+
+
+
+            //#region Assignment5
             //Create instance (Object) from (Employee) class [Physical representaion]
             //class name instance(object) with unique name
-            Employee employee = new Employee(); //reference point to employee(instance(object)) in memory in (heap)-->see next line to complete
-                                                //-->to separate eveysingle instance(object) name with others
-                                                //to determine some variable in some instance: instance name dot variable name| employee.EmployeeName
-                                                //note: we called variable the declare it inside class (member) not variable.
-            employee.ID = 1;
-            employee.Name = "Rayan Mohammed";
-            employee.address = "z city, A street, postcode: 12121";
-            employee.PhoneNumber = 0123456789;
-            employee.SetSalary(1);
-            employee.Display();
-            Console.WriteLine("=====================================\n");
+            // Employee employee = new Employee(); //reference point to employee(instance(object)) in memory in (heap)-->see next line to complete
+            //-->to separate eveysingle instance(object) name with others
+            //to determine some variable in some instance: instance name dot variable name| employee.EmployeeName
+            //                                    //note: we called variable the declare it inside class (member) not variable.
+            //employee.ID = 1;
+            //employee.Name = "Rayan Mohammed";
+            //employee.address = "z city, A street, postcode: 12121";
+            //employee.PhoneNumber = 0123456789;
+            //employee.SetSalary(1);
+            //employee.Display();
+            //Console.WriteLine("=====================================\n");
 
-            employee.addemployee();
-            #endregion
+            //employee.addemployee();
+            //#endregion
 
             //Console.WriteLine("Enter the first number:");
             //double num1 = Convert.ToDouble(Console.ReadLine());
@@ -113,109 +124,111 @@ namespace myFirstProject
             //Console.Write("]\n");
             //Console.WriteLine($"The number {specialNumber} occurs {count} times.");
 
+            //  }
+
+
+
+
+
+
+
+            //#region Assignment1
+            //#region declare the variables
+            //int a, b,sum,sub,mul,div;
+            //string n1,n2, c, d,fullname;
+            //#endregion
+
+            //#region + - * and / operations
+            //Console.WriteLine("Greetings, I'm calculator, Give me any two numbers and I will do my '+,-,* and /' operations and then give you the result!");
+            //Console.WriteLine("firstnumber:");
+            //n1 = Console.ReadLine();
+            //Console.WriteLine("secondnumber:");
+            //n2 = Console.ReadLine();
+            //a = Convert.ToInt32(n1);
+            //b = Convert.ToInt32(n2);
+            //sum = a + b;
+            //sub = a - b;
+            //mul = a * b;
+            //div = a / b;
+            //Console.WriteLine($"The result is showing as the following:-\n {a}+{b}={sum}\n {a}-{b}={sub}\n{a}*{b}={mul}\n{a}/{b}={div}");
+            //Console.WriteLine("Now let me show you some of my skills, give me your first and last name and I will give you the lehgth of your fullname!");
+            //Console.WriteLine("firstname:");
+            //c = Console.ReadLine();
+            //Console.WriteLine("lastname:");
+            //d = Console.ReadLine();
+            //fullname = c + d;  
+            //Console.WriteLine("The length of your full name fullname ="+ fullname.Length);
+            //#endregion
+            //#endregion
+
+            //#region Assignment 2
+
+            //decimal degree;
+            //Console.WriteLine("Enter Your degree to check if you're pass or fail:-");
+            //degree = Convert.ToDecimal(Console.ReadLine());
+
+            //if (degree >= 0 && degree <= 50)
+            //{
+            //    Console.WriteLine("(Fail):Try hard next time");
+            //}
+            //else if (degree >= 50 && degree <= 65)
+            //{
+            //    Console.WriteLine("(Fair):Fair enogh parctice more!");
+            //}
+            //else if (degree >=65 && degree <= 75)
+            //{
+            //    Console.WriteLine("(Good):Good you worked hard!");
+            //}
+            //else if (degree >=75 && degree <= 85)
+            //{
+            //    Console.WriteLine("(VeryGood): great!");
+            //}
+            //else if(degree >=85 && degree==100)
+            //{
+            //    Console.WriteLine("(Excelent):You are genius!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Incorrect degree!, try again.");
+            //}
+            //Console.WriteLine("-------------------------------------\nNothing hard but there's work hard to get what you want!.\n-------------------------------------");
+            //#endregion
+            //#region Assginment 3
+            //int choice;
+            //char switcher;
+
+            //do
+            //{
+            //    Console.WriteLine("Choose one of the following:-\n1) Good Morning. \n2) Good Evening. \n3) Good Afternoon. \n4) Good Night.");
+            //    choice = Convert.ToInt32(Console.ReadLine());
+            //    switch (choice)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Good Morning.");
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Good Evening.");
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Good Afternoon.");
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("Good Night.");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid Choice !.");
+            //            break;
+            //    }
+            //    Console.WriteLine("Do you want to continue?(y/n): ");
+            //    switcher = Convert.ToChar(Console.ReadLine());
+
+            //} while (switcher == 'y');
+            //#endregion
         }
 
 
 
-
-
-
-
-        //#region Assignment1
-        //#region declare the variables
-        //int a, b,sum,sub,mul,div;
-        //string n1,n2, c, d,fullname;
-        //#endregion
-
-        //#region + - * and / operations
-        //Console.WriteLine("Greetings, I'm calculator, Give me any two numbers and I will do my '+,-,* and /' operations and then give you the result!");
-        //Console.WriteLine("firstnumber:");
-        //n1 = Console.ReadLine();
-        //Console.WriteLine("secondnumber:");
-        //n2 = Console.ReadLine();
-        //a = Convert.ToInt32(n1);
-        //b = Convert.ToInt32(n2);
-        //sum = a + b;
-        //sub = a - b;
-        //mul = a * b;
-        //div = a / b;
-        //Console.WriteLine($"The result is showing as the following:-\n {a}+{b}={sum}\n {a}-{b}={sub}\n{a}*{b}={mul}\n{a}/{b}={div}");
-        //Console.WriteLine("Now let me show you some of my skills, give me your first and last name and I will give you the lehgth of your fullname!");
-        //Console.WriteLine("firstname:");
-        //c = Console.ReadLine();
-        //Console.WriteLine("lastname:");
-        //d = Console.ReadLine();
-        //fullname = c + d;  
-        //Console.WriteLine("The length of your full name fullname ="+ fullname.Length);
-        //#endregion
-        //#endregion
-
-        //#region Assignment 2
-
-        //decimal degree;
-        //Console.WriteLine("Enter Your degree to check if you're pass or fail:-");
-        //degree = Convert.ToDecimal(Console.ReadLine());
-
-        //if (degree >= 0 && degree <= 50)
-        //{
-        //    Console.WriteLine("(Fail):Try hard next time");
-        //}
-        //else if (degree >= 50 && degree <= 65)
-        //{
-        //    Console.WriteLine("(Fair):Fair enogh parctice more!");
-        //}
-        //else if (degree >=65 && degree <= 75)
-        //{
-        //    Console.WriteLine("(Good):Good you worked hard!");
-        //}
-        //else if (degree >=75 && degree <= 85)
-        //{
-        //    Console.WriteLine("(VeryGood): great!");
-        //}
-        //else if(degree >=85 && degree==100)
-        //{
-        //    Console.WriteLine("(Excelent):You are genius!");
-        //}
-        //else
-        //{
-        //    Console.WriteLine("Incorrect degree!, try again.");
-        //}
-        //Console.WriteLine("-------------------------------------\nNothing hard but there's work hard to get what you want!.\n-------------------------------------");
-        //#endregion
-        //#region Assginment 3
-        //int choice;
-        //char switcher;
-
-        //do
-        //{
-        //    Console.WriteLine("Choose one of the following:-\n1) Good Morning. \n2) Good Evening. \n3) Good Afternoon. \n4) Good Night.");
-        //    choice = Convert.ToInt32(Console.ReadLine());
-        //    switch (choice)
-        //    {
-        //        case 1:
-        //            Console.WriteLine("Good Morning.");
-        //            break;
-        //        case 2:
-        //            Console.WriteLine("Good Evening.");
-        //            break;
-        //        case 3:
-        //            Console.WriteLine("Good Afternoon.");
-        //            break;
-        //        case 4:
-        //            Console.WriteLine("Good Night.");
-        //            break;
-        //        default:
-        //            Console.WriteLine("Invalid Choice !.");
-        //            break;
-        //    }
-        //    Console.WriteLine("Do you want to continue?(y/n): ");
-        //    switcher = Convert.ToChar(Console.ReadLine());
-
-        //} while (switcher == 'y');
-        //#endregion
     }
-
-
-
 }
+
 
